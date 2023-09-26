868ddfb7fc17de6a25b5e06b19e17582a48eb962
supragirl100@gmail.com <supragirl100@gmail.com>
2023-09-25 23:56:40 -0400

    UTILS

---
+++ Utils/Data.js
@@ -0,0 +1,29 @@
+New user...
+{
+    "username": "user@",
+    "email": "user@@users.com"
+},
+{
+    "username": "user!",
+    "email": "user!@users.com"
+},
+
+Updated User
+{
+    "username": "user@updated",
+    "email": "user@@users.com"
+},
+
+
+Create Thought...
+{
+"thoughtText": "...and a thought...",
+"username": "user1",
+"userId": ""
+},
+
+Create reaction
+{
+"reactionBody":"A reaction",
+"username":"user1"
+},
---
+++ Utils/Seed.js
@@ -0,0 +1,32 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+const { users } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+  // Delete the collections if they exist
+  let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+  if (thoughtCheck.length) {
+    await connection.dropCollection('thoughts');
+  }
+  
+  let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+  if (userCheck.length) {
+    await connection.dropCollection('users');
+  }
+
+
+  // const thoughts = getRandomThoughts(10);
+
+
+  await User.collection.insertMany(users);
+  // await Thought.collection.insertMany(thoughts);
+
+  // loop through the saved applications, for each application we need to generate a application response and insert the application responses
+  console.table(users);
+  // console.table(thoughts);
+  console.info('Seeding done');
+  process.exit(0);
+});
